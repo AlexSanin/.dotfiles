3c159b767c6f6cdf4c030dbb98e9f9cd51ef7b12
Oleksii Sanin <letsgetlift@gmail.com>
2024-04-07 09:15:13 +0300

    added new configs

---
+++ .config/nvim/after/plugin/test.lua
@@ -0,0 +1,12 @@
+--require('neotest').setup({
+--  adapters = {
+--    require('neotest-jest')({
+--      jestCommand = "npm run test:react",
+--      jestConfigFile = "jest.config.js",
+--      env = { CI = true },
+--      cwd = function(path)
+--        return vim.fn.getcwd()
+--      end,
+--    }),
+--  }
+--})
